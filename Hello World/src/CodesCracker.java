60922d674a338203aae5fe813382800b5fb81825
Jenoveva01 <167199272+Jenoveva01@users.noreply.github.com>
2024-05-15 21:33:49 +0300

    Add files via upload
    add two numbers

--- Hello World/src/CodesCracker.java
+++ Hello World/src/CodesCracker.java
@@ -1,13 +1,21 @@
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+
 public class CodesCracker {
+    int add;
+    CodesCracker(int x,int y){
+        add =x + y;
+        System.out.println("\nResult =" +add);
+    }
     public static void main(String[] args)
     {
-        int n;
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter the value of n");
-        n = s.nextInt();
 
-        for( int i=0; i<n; i++)
-            System.out.println("Hello World");
+        System.out.print("Enter the first number:");
+        int a = s.nextInt();
+        System.out.print("Enter the second number:");
+        int b = s.nextInt();
+
+        CodesCracker obj = new CodesCracker(a, b);
     }
 }
